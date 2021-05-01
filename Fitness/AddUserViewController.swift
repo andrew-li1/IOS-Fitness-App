7eeb549e19b63470575861396d377a59c1f6a737
Andrew Li <li24@sas.upenn.edu>
2021-04-30 22:22:32 -0400

    Added login feature using Firebase Auth

---
+++ Fitness/AddUserViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AddUserViewController.swift
+//  Fitness
+//
+//  Created by Jiacheng Li on 4/29/21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class AddUserViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        errorLabel.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createUser(_ sender: Any) {
+        let email = emailText.text!
+        let password = passwordText.text!
+        
+        
+        Auth.auth().createUser(withEmail: email, password: password, completion: {(result, err) in
+            if err != nil {
+                self.errorLabel.isHidden = false
+                print(err!)
+            } else {
+                print("created user")
+                self.dismiss(animated: true, completion: nil)
+            }
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fitness/SignInViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SignInViewController.swift
+//  Fitness
+//
+//  Created by Jiacheng Li on 4/29/21.
+//
+
+import UIKit
+import Firebase
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        errorMessage.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        let email = emailText.text!
+        let password = passwordText.text!
+        
+        Auth.auth().signIn(withEmail: email, password: password, completion: {(result, err) in
+            if err != nil {
+                self.errorMessage.isHidden = false
+                print(err!)
+            } else {
+                print("Successful Login")
+                self.performSegue(withIdentifier: "tableViewSegue", sender: self)
+            }
+        })
+        
+        
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "addUserSeque", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
